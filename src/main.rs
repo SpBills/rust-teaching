b41d625b591fce36d260b1e3729175275771c695
spbills <spencebills0@gmail.com>
2021-11-29 11:34:09 -0500

    init

---
+++ src/main.rs
@@ -0,0 +1,33 @@
+/// START EXAMPLE 1
+// struct User {
+//     name: String,
+//     password: String
+// }
+
+// impl User {
+//     fn print_user(&self) {
+
+//     }
+// }
+/// END EXAMPLE 1
+
+/// START EXAMPLE 2
+// fn prepend_text(input: String, new_text: String) {
+//     input = format!("{}{}", new_text, input);
+// }
+// END EXAMPLE 2
+
+fn main() {
+    // EXAMPLE 1
+    // let user = User {
+    //     name: "",
+    //     password: ""
+    // };
+    // user.print_user();
+
+    // EXAMPLE 2
+    // let source = String::from("world!");
+    // let prepend = String::from("Hello ");
+    // prepend_text(prepend, source);
+    // println!("{}", source);
+}
